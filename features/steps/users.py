a6d0ffc4e551d06060da545d32a4a0ff2cc14b68
ericminio <eric.mignot@gmail.com>
2021-03-28 08:55:52 -0700

    hamcrest

--- features/steps/users.py
+++ features/steps/users.py
@@ -1,16 +1,17 @@
 from behave import given, when, then
+from hamcrest import assert_that, equal_to
 
 @then('I see the user list')
 def users(context):
     elements = context.browser.find_by_css('#users')
-    assert len(elements) > 0
+    assert_that(len(elements) > 0, equal_to(True))
 
 @then('I see the first user is "{name}"')
 def first_user(context, name):
     element = context.browser.find_by_css('#users .user:nth-child(1)').first
-    assert element.text == name
+    assert_that(element.text, equal_to(name))
 
 @then('I see the second user is "{name}"')
 def second_user(context, name):
     element = context.browser.find_by_css('#users .user:nth-child(2)').first
-    assert element.text == name
+    assert_that(element.text, equal_to(name))
