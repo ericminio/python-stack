72b81e6f480e007d67f30a8ead1b291e63b684cb
Eric Mignot <eric.mignot@gmail.com>
2021-04-05 14:50:32 -0700

    router component

---
+++ nginx/public/js/router.js
@@ -0,0 +1,20 @@
+window.customElements.define('hw-router', class HwRouter extends HTMLElement {
+    static get observedAttributes() {
+        return ['content'];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.content = newValue
+    }
+    constructor() {
+        super();
+        this.routes = [
+            { match:(request)=> request == '?page=users', html:`<hw-users></hw-users>` },
+            { match:()=> true, html:`<hw-home></hw-home>` }
+        ]
+    }
+    connectedCallback() {
+        let request = window.location.search
+        let app = document.getElementById(this.content)
+        app.innerHTML = this.routes.find(route => route.match(request)).html
+    }
+})
